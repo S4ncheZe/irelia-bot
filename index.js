860a7112cfaf35d5c8315fe2d31364b2c80ea481
S4ncheZe <38075909+S4ncheZe@users.noreply.github.com>
2018-04-04 22:54:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var prefix = "!";
+var x = 0;
+
 const Discord = require('discord.js');
 const robot = new Discord.Client();
 robot.login("NDMwNzkwMzY5ODQ0OTIwMzMw.DaVUaQ.svtc8maxYOOe7Wpgy2SFA00lrnc");
@@ -12,11 +15,11 @@ robot.on('guildMemberAdd', function(member) {
 
 robot.on('message', message =>{
       
-  if(message.content == "Денис"){
-    message.reply("Люблю Дениса <3");
+  if(message.content == "Что скажешь о Денисе?"){
+    message.channel.sendMessage("Люблю Дениса <3");
   }
-  if(message.content == "Протеин"){
-    message.reply(" Протеин + Лулу = CENSORED");
+  if(message.content == "Что скажешь о Протеине?"){
+    message.channel.sendMessage(" Протеин + Лулу = CENSORED");
   }
 
 })
@@ -24,4 +27,27 @@ robot.on('message', message =>{
 robot.on('ready', () => {
     console.log('I am ready!');
   });
-  
+
+  robot.on('message', message =>{
+    
+
+
+    var words1 = ["Ясуо", "Шая", "Вел'Коз", "Зед", "Рэйкан", "Азир", "Акали", "Алистар", "Азир", "Акали", "Алистар", "Амуму", "Анивия", "Ари", "Атрокс", "Аурелион Сол", "Бард"];
+       
+        var rand1 = Math.floor(Math.random() * words1.length);
+        var phrase = words1[rand1];
+        var p1, p2, p3, p4, p5;
+
+       if(message.content == prefix + "герой"){
+         message.channel.sendMessage("Твой герой на эту игру: " + phrase);
+       }
+
+       while(x == 5){
+         if(message.content == "лес" || message.content == "топ" || message.content == "сап", "саппорт" || message.content == "адк" || message.content == "мид"){
+            p1 = message.author;
+            console.log(p1);
+         }
+       }
+     
+               
+  })

4fc4d7097e6919066af0dd0d1098e35fc3c09bbe
S4ncheZe <38075909+S4ncheZe@users.noreply.github.com>
2018-04-04 17:59:03 +0300

    Create irelia.js

---
+++ irelia.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const robot = new Discord.Client();
+robot.login("NDMwNzkwMzY5ODQ0OTIwMzMw.DaVUaQ.svtc8maxYOOe7Wpgy2SFA00lrnc");
+
+robot.on('guildMemberAdd', function(member) {
+
+  member.guild.channels.find("name", "общение").sendMessage(member.toString() + ", привет. Сейчас твоя роль в игре не определена, свяжись с основателем канала, чтобы у тебя появилась роль)")
+
+  member.addRole(member.guild.roles.find("name", "Ещё не определен"));
+
+});
+
+robot.on('ready', () => {
+    console.log('I am ready!');
+  });
+  
